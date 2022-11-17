7af37fb7e0c7214cb1231700ea4d0743c0b3c2c0
Sehan, Kim <stanley710@naver.com>
2022-11-17 22:47:19 +0900

    initiate server
    all npm modules installed,
    check api /info

---
+++ server/service/fetchLiveData.js
@@ -0,0 +1,26 @@
+import { API_URL } from "../static/API_config.js"
+import { toJson } from 'xml2json';
+import fetch from 'node-fetch';
+
+//장소별 실시간 정보 + XML 파싱 to JSON
+export const fetchLiveData = async (placeName) => {
+    const response = await fetch(API_URL(placeName))
+                .then((res) => res.text())
+                .then((xml) => toJson(xml, {
+                        object: true, //반환값 object로 변환
+                        reversible: false,
+                        coerce: false,
+                        sanitize: true,
+                        trim: true,
+                        arrayNotation: false,
+                        alternateTextNode: false
+                        }))
+                .then((obj) => {
+                    const seoulcitydata = Object.values(obj)[0]
+                    return {
+                        name : placeName,
+                        citydata : seoulcitydata.CITYDATA.LIVE_PPLTN_STTS
+                    }
+                })
+    return response;
+}
